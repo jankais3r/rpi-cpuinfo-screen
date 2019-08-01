bdc5f4e90d9980b2f0c0278d89f7f099c7cc9ae3
Jan Kaiser <2159464+jankais3r@users.noreply.github.com>
2019-08-02 00:15:56 +0200

    Fixed an error in parsing uptime
    Fixed an error in parsing of the uptime string when the value is between 1 day and 1 day 1 hour

--- status.sh
+++ status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-UPTIME=`uptime | awk -F'( |,|:)+' '{if ($7=="min") m=$6; else {if ($7~/^day/) {d=$6;h=$8;m=$9} else {h=$6;m=$7}}} {print d+0,"d",h+0,"h",m+0,"m"}'`
+UPTIME=`uptime | awk -F'( |,|:)+' '{if ($7=="min") {d=0;h=0;m=$6} else {if ($7~/^day/ && $9=="min") {d=$6;h=0;m=$8} else if ($7~/^day/) {d=$6;h=$8;m=$9} else {d=0;h=$6;m=$7}}}{print d+0,"d",h+0,"h",m+0,"m"}'`
 CPUUSE=`vmstat 1 2 | tail -1 | awk '{print $13+$14}'`
 CPUTEMP=`echo "$(($(</sys/class/thermal/thermal_zone0/temp)/1000))'C"`
 USEDRAM=`free | grep Mem | awk '{print $3/$2 * 100.0}' | awk '{printf "%.f\n", int($1)}'`
